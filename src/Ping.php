c281dcd4d35ca4e6b356704b8a335a146728f07a
Sterling R <sterling.ross@nearo.com>
2016-09-08 10:30:33 -1000

    Added comment

--- src/Ping.php
+++ src/Ping.php
@@ -7,6 +7,7 @@ class Ping
 
     public function sendPing($host)
     {
+        //TODO update regex to valiadate hostname
         $host = preg_replace("/[^A-Za-z0-9.]/", "", $host);
 
         $output =  shell_exec("ping -c 4 " . escapeshellarg($host) . " 2>&1");
