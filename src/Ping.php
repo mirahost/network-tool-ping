e79220236151f985c508657324ef353ca7eb18a1
Sterling R <sterling.ross@nearo.com>
2016-09-08 10:04:16 -1000

    Updated command output to show any errors

--- src/Ping.php
+++ src/Ping.php
@@ -9,7 +9,7 @@ class Ping
     {
         $host = preg_replace("/[^A-Za-z0-9.]/", "", $host);
 
-        $output =  shell_exec("ping -c 4 " . escapeshellarg($host));
+        $output =  shell_exec("ping -c 4 " . escapeshellarg($host) . " 2>&1");
 
         return $output;
     }
